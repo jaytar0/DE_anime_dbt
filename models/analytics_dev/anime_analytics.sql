d0facd5b02e5d4d1b16ca220542a5b95ea8324bb
Jonathan Feng <winterlemonmelon@gmail.com>
2023-02-21 22:29:30 +0000

    datefix

--- models/analytics_dev/anime_analytics.sql
+++ models/analytics_dev/anime_analytics.sql
@@ -12,7 +12,7 @@ with anime_analytics AS (
         rating,
         rank,
         release_season,
-        TO_TIMESTAMP(TO_NUMERIC(release_year)) AS release_year,
+        TO_DATE(CONCAT(TO_CHAR(CAST(release_year AS INTEGER)), '0101'), 'YYYYMMDD') AS release_year,
         related_manga,
         voice_actors,
         staff,
